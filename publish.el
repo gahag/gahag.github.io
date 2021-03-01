9589837fd2a184f3d315051b32019a09d6433fb9
gahag <gabriel.s.b@live.com>
2021-03-01 03:19:21 -0300

    Update wording

--- publish.el
+++ publish.el
@@ -98,7 +98,7 @@ PROJECT: `posts in this case."
          :exclude ,(regexp-opt '("README.org" "draft"))
          :auto-sitemap t
          :sitemap-filename "index.org"
-         :sitemap-title "Blog Index"
+         :sitemap-title "Tales"
          :sitemap-format-entry org-sitemap-format-entry
          :sitemap-style list
          :sitemap-sort-files anti-chronologically
