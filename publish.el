eca29c35dc3b3395bb00a3b02a8ab3ee7ab7be09
gahag <gabriel.s.b@live.com>
2021-03-18 04:05:11 -0300

    Update template

--- publish.el
+++ publish.el
@@ -35,7 +35,7 @@
 
 
 (defvar website-html-head
-  "<link rel='icon' type='image/x-icon' href='/images/favicon.ico'/>
+  "<link rel='icon' type='image/png' href='/images/favicon.png'/>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link rel='preconnect' href='https://fonts.gstatic.com'>
 <link href='https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@300&display=swap' rel='stylesheet'>
