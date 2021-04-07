6628e59fcba2c67d90ccae33b400a2375b014e8d
gahag <gabriel.s.b@live.com>
2021-04-07 00:48:46 -0300

    Install packages in publish.el for github actions

--- publish.el
+++ publish.el
@@ -11,6 +11,16 @@
 
 ;;; Code:
 (package-initialize) ;; Required because we need to load htmlize.
+
+(unless package-archive-contents
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents))
+(dolist (pkg '(htmlize))
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
 (require 'org)
 (require 'ox-publish)
 
